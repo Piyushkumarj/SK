571554aea3cc1fdee09a17948be7417cc74ba0a1
PIYUSH JAISWAL <120399897+Piyushkumarj@users.noreply.github.com>
2024-10-28 19:31:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    await customElements.whenDefined('gmpx-store-locator');
-    const locator = document.querySelector('gmpx-store-locator');
-    locator.configureFromQuickBuilder(CONFIGURATION);
-  });
+
